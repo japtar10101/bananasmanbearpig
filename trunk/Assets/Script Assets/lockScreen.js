3d3861302e7d54edcb155967b52f7965fa195d9b
eltodd <eltodd@b497f940-11a0-11df-96c7-376cde75ada7>
2010-02-08 03:04:22 +0000

    Updates
    Objects turn red when in gravity gun range
    Mouse is locked
    Mouse is hidden
    Gravity Gun range is finite
    Gravity Gun strength has been increased
    Low quality cursor drawn at center of screen

---
+++ trunk/Assets/Script Assets/lockScreen.js
@@ -0,0 +1,3 @@
+function Update () {
+	Screen.lockCursor = true;
+}
---
+++ trunk/Assets/Script Assets/restoreColor.js
@@ -0,0 +1,5 @@
+function Update () {
+   renderer.material.color.r = 1.0; 
+   renderer.material.color.g = 1.0; 
+   renderer.material.color.b = 1.0; 
+}
