8636722a83bfb2421c1c439d67400b8524b1a6fa
Japtar10101 <Japtar10101@b497f940-11a0-11df-96c7-376cde75ada7>
2010-02-09 15:00:41 +0000

    Good news!  I got the repulsion working!

--- Assets/Script Assets/gravityGun.js
+++ Assets/Script Assets/gravityGun.js
@@ -5,26 +5,28 @@ var angularDrag = 5.0;
 var springMaxDistance = 0.5;
 var detectionDistance = 30;
 var attractionDistance = 3;
+var repulsionDistance = 10;
 
 private var springJoint : SpringJoint;
 
 function Update ()
 {
-	// Make sure the user pressed the mouse down
-
-	var mainCamera = FindCamera();
-		
 	// We need to actually hit an object
+	var mainCamera = FindCamera();
 	var hit : RaycastHit;
 	if (!Physics.Raycast(mainCamera.ScreenPointToRay(Vector3(mainCamera.pixelWidth/2, mainCamera.pixelHeight/2,0)),  hit, detectionDistance))
 		return;
+	
 	// We need to hit a rigidbody that is not kinematic
 	if (!hit.rigidbody || hit.rigidbody.isKinematic)
 		return;
+	
 	// Turn the object red
 	hit.rigidbody.renderer.material.color.r = 1.0; 
 	hit.rigidbody.renderer.material.color.g = 0.0; 
-	hit.rigidbody.renderer.material.color.b = 0.0; 
+	hit.rigidbody.renderer.material.color.b = 0.0;
+	
+	// Make sure the user pressed the mouse down
 	if (!Input.GetMouseButtonDown(0)) return;	
 	if (!springJoint)
 	{
@@ -42,24 +44,33 @@ function Update ()
 	springJoint.maxDistance = springMaxDistance;
 	springJoint.connectedBody = hit.rigidbody;
 	
-	StartCoroutine ("DragObject", attractionDistance);
+	StartCoroutine ("Gravity");
 }
 
-function DragObject ()
+function Gravity ()
 {
 	var oldDrag = springJoint.connectedBody.drag;
 	var oldAngularDrag = springJoint.connectedBody.angularDrag;
 	springJoint.connectedBody.drag = drag;
 	springJoint.connectedBody.angularDrag = angularDrag;
 	var mainCamera = FindCamera();
+	var ray : Ray;
+	
+	//Loop while holding he left button
 	while (Input.GetMouseButton (0))
 	{
-		var ray = mainCamera.ScreenPointToRay (Vector3(mainCamera.pixelWidth/2, mainCamera.pixelHeight/2,0));
+		ray = mainCamera.ScreenPointToRay (Vector3(mainCamera.pixelWidth/2, mainCamera.pixelHeight/2,0));
 		springJoint.transform.position = ray.GetPoint(attractionDistance);
 		yield;
 	}
 	if (springJoint.connectedBody)
 	{
+		// repel the object once we stop holding the mouse button
+		ray = mainCamera.ScreenPointToRay (Vector3(mainCamera.pixelWidth/2, mainCamera.pixelHeight/2,0));
+		springJoint.transform.position = ray.GetPoint(repulsionDistance);
+		yield;
+		
+		// remove the joints
 		springJoint.connectedBody.drag = oldDrag;
 		springJoint.connectedBody.angularDrag = oldAngularDrag;
 		springJoint.connectedBody = null;
