2e8d62f58073a92b28fd0b83405a8634504a101d
Japtar10101 <Japtar10101@b497f940-11a0-11df-96c7-376cde75ada7>
2010-02-11 22:30:09 +0000

    Adding levelSelecter.js in confined.unity.  This is our main menu: the LEVEL SELECTOR!!! RAR!!

--- trunk/Assets/Script Assets/levelSelecter.js
+++ trunk/Assets/Script Assets/levelSelecter.js
@@ -1,7 +1,12 @@
-var levelNames : String[testing, gap, confined];
+var levelNames : String[] = ["testing", "gap"];
 var title : String = "Select a level";
-var groupWidth : int = 400;
-var groupHeight : int = 500;
+var buttonHeight : int = 30;
+var buttonGap : int = 10;
+var buttonWidth : int = 80;
+
+private var numLevels : int = levelNames.length;
+private var groupWidth : int = buttonWidth + 20;
+private var groupHeight : int = buttonHeight * (numLevels + 1) + buttonGap * (numLevels + 1);
 
 function OnGUI () {
 	var shiftLeft : int = Screen.width / 2 - groupWidth / 2;
@@ -12,8 +17,17 @@ function OnGUI () {
 	// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
 
 	// We'll make a box so you can see where the group is on-screen.
-	GUI.Box (Rect (0,0,100,100), title);
-	GUI.Button (Rect (10,40,80,30), "Click me");
+	GUI.Box (Rect (0,0,groupWidth,groupHeight), title);
+	
+	// Position all the buttons
+	var y = buttonHeight; var x = 10;
+	for( var i = 0; i < numLevels; ++i ) {
+		var levelName = levelNames[i];
+		if(GUI.Button (Rect (x, y,buttonWidth,buttonHeight), levelName)) {
+			Application.LoadLevel(levelName);
+		}
+		y += buttonHeight + buttonGap;
+	}
 
 	// End the group we started above. This is very important to remember!
 	GUI.EndGroup ();
