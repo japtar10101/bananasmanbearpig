3cb7872c9688a51ec25c64b9414cd181cc0c3f13
Japtar10101 <Japtar10101@b497f940-11a0-11df-96c7-376cde75ada7>
2010-02-11 23:09:45 +0000

    Adding the crate of awesome in testing.unity

---
+++ Assets/Script Assets/win.js
@@ -0,0 +1,7 @@
+
+function Update () {
+	// Grab the controller's location
+	var controller : CharacterController = GetComponent(CharacterController);
+}
+
+@script RequireComponent(CharacterController)
