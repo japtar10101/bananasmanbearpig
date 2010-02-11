11ee4c98dc5bd0deed817acc7868664b721c8212
Japtar10101 <Japtar10101@b497f940-11a0-11df-96c7-376cde75ada7>
2010-02-11 22:00:38 +0000

    Taking away the grappling hook object in confined.unity and testing.unity

--- Assets/Script Assets/levelSelecter.js
+++ Assets/Script Assets/levelSelecter.js
@@ -1,5 +1,20 @@
+var levelNames : String[testing, gap, confined];
+var title : String = "Select a level";
+var groupWidth : int = 400;
+var groupHeight : int = 500;
+
 function OnGUI () {
-	if (GUI.Button (Rect (10,10,150,100), "I am a button")) {
-		print ("You clicked the button!");
-	}
+	var shiftLeft : int = Screen.width / 2 - groupWidth / 2;
+	var shiftUp : int = Screen.height / 2 - groupHeight / 2;
+
+	// Make a group on the center of the screen
+	GUI.BeginGroup (Rect (shiftLeft, shiftUp, groupWidth, groupHeight));
+	// All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
+
+	// We'll make a box so you can see where the group is on-screen.
+	GUI.Box (Rect (0,0,100,100), title);
+	GUI.Button (Rect (10,40,80,30), "Click me");
+
+	// End the group we started above. This is very important to remember!
+	GUI.EndGroup ();
 }
