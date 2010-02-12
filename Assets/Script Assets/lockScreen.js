c1ae3b98a35a2915af5ccbf97ee43ecd4283a758
Japtar10101 <Japtar10101@b497f940-11a0-11df-96c7-376cde75ada7>
2010-02-12 00:14:34 +0000

    Updated GUI components.

--- Assets/Script Assets/lockScreen.js
+++ Assets/Script Assets/lockScreen.js
@@ -1,5 +1,6 @@
 
 private var locked = true;
+private var showMenu = false;
 private var lastclicked = false;
 
 
@@ -7,6 +8,7 @@ function Update () {
 	if(Input.GetKey("p") && !lastclicked){
 		locked = !locked;
 		lastclicked = true;
+		showMenu = !showMenu;
 	}
 	if(!Input.GetKey("p")){
 		lastclicked = false;
@@ -17,21 +19,22 @@ function Update () {
 }
 
 function OnGUI(){
-	var RectWidth = 100;
-	var RectHeight = 90;
+	var RectWidth = Screen.width/2;
+	var RectHeight = Screen.height/2;
+	var buttonHeight = RectHeight / 3;
 	var RectY = Screen.height/2 - RectHeight/2;
 	var RectX = Screen.width/2 - RectWidth/2;
-	if(!locked){
+	if(showMenu){
 		// Make a background box
-		GUI.Box (Rect (RectX,RectY,100,90), "Pause Menu");
+		GUI.Box (Rect (RectX,RectY,RectWidth,RectHeight), "Pause Menu");
 
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if (GUI.Button (Rect (RectX+10,RectY+30,RectWidth-20,20), "Reload")) {
+		if (GUI.Button (Rect (RectX+10,RectY+30,RectWidth-20,buttonHeight), "Reload")) {
 			Application.LoadLevel (Application.loadedLevel);
 		}
 
 		// Make the second button.
-		if (GUI.Button (Rect (RectX+10,RectY+60,RectWidth-20,20), "Level Select")) {
+		if (GUI.Button (Rect (RectX+10,RectY+buttonHeight + 60,RectWidth-20,buttonHeight), "Level Select")) {
 			Application.LoadLevel ("confined");
 		}
 	}	
--- Assets/Script Assets/win.js
+++ Assets/Script Assets/win.js
@@ -1,7 +1,33 @@
+private var win : boolean = false;
+private var controller : CharacterController;
 
-function Update () {
+function OnTriggerEnter (other : Collider) {
 	// Grab the controller's location
-	var controller : CharacterController = GetComponent(CharacterController);
+	if(other.GetComponent(gravityGun)) {
+		win = true;
+		var lockedScreen = GetComponent(lockScreen);
+		lockedScreen.locked = false;
+		//Screen.lockCursor =  false;
+	}
+}
+
+function OnGUI() {
+	if(win) {
+		var RectWidth = Screen.width/2;
+		var RectHeight = Screen.height/2;
+		var ButtonWidth = RectWidth - 20;
+		var ButtonHeight = RectHeight - 40;
+		var RectY = Screen.height/2 - RectHeight/2;
+		var RectX = Screen.width/2 - RectWidth/2;
+	
+		// Make a background box
+		GUI.Box (Rect (RectX,RectY,RectWidth,RectHeight), "Congratulations!");
+		
+		// Make the OK button.
+		if (GUI.Button (Rect (RectX+10,RectY+30,ButtonWidth,ButtonHeight), "OK")) {
+			Application.LoadLevel ("confined");
+		}
+	}
 }
 
 @script RequireComponent(CharacterController)
